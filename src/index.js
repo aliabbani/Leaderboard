24a2310d4b59a9c6703c9407e32da8be8e25a346
ali abbani <aliox@ahlam.com>
2021-08-31 13:43:24 +0300

    Add js results to show in the ui & fix css basics

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import _ from 'lodash';
 import './style.css';
+
+const results = [
+  {
+    name: 'Ali',
+    score: '100',
+  },
+  {
+    name: 'Henry',
+    score: '100',
+  },
+  {
+    name: 'Elyor',
+    score: '100',
+  },
+  {
+    name: 'Zeenat',
+    score: '100',
+  },
+  {
+    name: 'Faizii',
+    score: '100',
+  },
+  {
+    name: 'Elmar',
+    score: '100',
+  },
+];
+
+const table = document.querySelector('.table');
+const yourName = document.getElementById('yourName');
+const yourScore = document.getElementById('yourScore');
+const submit = document.getElementsByClassName('submit');
+
+function generateResult() {
+  results.forEach((result) => {
+    table.innerHtml = '';
+    const content = `${result.name}: ${result.score}`;
+
+    const list = document.createElement('div');
+    list.className = 'list';
+    list.innerText = content;
+
+    table.appendChild(list);
+  });
+}
+
+generateResult();
